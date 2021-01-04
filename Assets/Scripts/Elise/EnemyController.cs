48d0fafb0cd3f9b104b9a2106e81e6156cd0f354
Elise-Nguyen <55691760+Elise-Nguyen@users.noreply.github.com>
2021-01-04 16:43:08 +0100

    Pack enemi qui peut tuer le joueur

---
+++ Assets/Scripts/Elise/EnemyController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour
+{
+    private Transform enemyHolder;
+    public float speed;
+
+    public GameObject shot;
+    public float fireRate = 0.997f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MoveEnemy", 0.1f, 0.3f);
+        enemyHolder = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void MoveEnemy()
+    {
+        enemyHolder.position += Vector3.right * speed;
+
+        foreach(Transform enemy in enemyHolder)
+        {
+            if(enemy.position.x < -10f || enemy.position.x > 10f)
+            {
+                speed = -speed;
+                enemyHolder.position += Vector3.down * 0.5f;
+                return;
+            }
+
+            //Random fire shoot
+            if(Random.value > fireRate)
+            {
+                Instantiate(shot, enemy.position, enemy.rotation);
+            }
+
+
+            if(enemy.position.y <= -4)
+            {
+
+            }
+        }
+
+        if(enemyHolder.childCount == 1)
+        {
+            CancelInvoke();
+            InvokeRepeating("MoveEnemy", 0.1f, 0.25f);
+        }
+
+        if(enemyHolder.childCount == 0)
+        {
+
+        }
+    }
+}
+ 
