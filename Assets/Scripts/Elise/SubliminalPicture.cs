7f09bf6b733859cc7a562b3cb5d3d721ac1e6fcb
Elise-Nguyen <55691760+Elise-Nguyen@users.noreply.github.com>
2021-01-07 11:50:41 +0100

    Ajout de l'image subiminale

---
+++ Assets/Scripts/Elise/SubliminalPicture.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubliminalPicture : MonoBehaviour
+{
+
+    public bool isDisplay = false;
+    public float displayTimae = 0.5f;
+    private float time;
+    [SerializeField] private Image sublimImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sublimImage.enabled = false;
+        time = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Image is display");
+            isDisplay = true;
+        }
+        */
+
+        if (isDisplay)
+        {
+            sublimImage.enabled = true;
+            time += Time.deltaTime;
+            if (time >= displayTimae)
+            {
+                isDisplay = false;
+                time = 0f;
+            }
+        }
+        else
+        {
+            sublimImage.enabled = false;
+        }
+
+    }
+}
